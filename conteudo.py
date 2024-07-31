a7a503dab8275d695d5ca1cfb46a0f8e919d0aba
eduardoandd <eduardo.andrade.dev@gmail.com>
2024-07-31 12:02:49 -0300

    first commit

---
+++ conteudo.py
@@ -0,0 +1,16 @@
+#Técnicas de Mineração de texto
+
+#1 - Classificação: Recebe uma entrada, o algoritmo(classificador) faz a análise da entrada e ta da uma saída.
+#2 - Agrupamentos(Clustering): Colocar diferentes objetos na mesma categoria.
+#3 - Extração da informação: Análisar informação e extrair informações utilizando conceitos de relacionamento.
+#4 - Associações(correlação entre palavras): associar quando determinadas palavras estão juntas e previr oq vai acontecer
+#5 - Casamento de esquemas: correspondência semantica
+#6 - Recuperação da Informação: localizar e ranquer documentos relevantes em uma coleção.
+#7 - Sumarização de documentos: faz o resumo do documento.
+#8 - Estatistica: frequêmcia dos termos, ignorando informações semânticas
+#9 Processamento de linguagem natural(pln): interpretação sintática e semântica das frases, fazer o pc entender textos escritos em linguagem huamana
+
+
+# Mineração por Emoção
+#1 - Classificação: Recebe uma entrada e o algoritimo faz análise dessa entrada e classifica a emoção que a entrada transmite
+#2 - Classificação por polaridade: retorna se é um texto positivo,neutro, negativo 
